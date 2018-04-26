b938e5076e9f04f792dd85bb05cdb6ec1a307c72
zsolthomework <zsolt@mail.hu>
2018-04-26 13:21:59 +0200

    Before going github

--- src/main/java/com/matritellabs/utama/Game.java
+++ src/main/java/com/matritellabs/utama/Game.java
@@ -1,4 +1,29 @@
 package main.java.com.matritellabs.utama;
+import main.java.com.matritellabs.utama.helper.*;
+
 
 public class Game {
+
+    public static void main (String[] args) {
+
+        Table gameBoard = new Table();
+        Battleships shipToPlace = new Battleships();
+
+        LineByLineReader toRead = new LineByLineReader();
+        String coorToRead = toRead.readLineFromStdIn();
+        String direction = toRead.readLineFromStdIn();
+        String gamer1 = toRead.readLineFromStdIn();
+
+        System.out.println("Please type first player's name: ");
+
+        Player player1 = new Player(gamer1, gameBoard);
+
+        boolean allplaced = false;
+        while (!allplaced) {
+            Battleships notplaced = player1.notplacedyet();
+            System.out.println("Write your " + notplaced + "'s coordinate and direction");
+            player1.battleshipPlace(coorToRead, direction, shipToPlace);
+            allplaced = player1.checkIfAllPlaced();
+        }
+    }
 }
