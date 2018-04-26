869eac575ae11c510671f1be6f69f55e8bfb12ae
zsolthomework <zsolt@mail.hu>
2018-04-26 14:24:19 +0200

    After github

--- src/main/java/com/matritellabs/utama/Game.java
+++ src/main/java/com/matritellabs/utama/Game.java
@@ -6,17 +6,17 @@ public class Game {
 
     public static void main (String[] args) {
 
-        Table gameBoard = new Table();
-        Battleships shipToPlace = new Battleships();
+        ScoreTable myScore = new ScoreTable();
+        BattleshipTable toPlay = new BattleshipTable();
 
         LineByLineReader toRead = new LineByLineReader();
         String coorToRead = toRead.readLineFromStdIn();
         String direction = toRead.readLineFromStdIn();
-        String gamer1 = toRead.readLineFromStdIn();
+        String gamerNameFromKeyboard = toRead.readLineFromStdIn();
 
         System.out.println("Please type first player's name: ");
 
-        Player player1 = new Player(gamer1, gameBoard);
+        Player player1 = new Player(gamerNameFromKeyboard);
 
         boolean allplaced = false;
         while (!allplaced) {
