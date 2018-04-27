f14855b65ff1b5d99a090d9ef3a749f63df92f11
mgergo <mandi.gergo@gmail.com>
2018-04-27 15:49:04 +0200

    Last commit on Friday

--- src/main/java/com/matritellabs/utama/Table.java
+++ src/main/java/com/matritellabs/utama/Table.java
@@ -10,34 +10,41 @@ public abstract class Table {
 
     /////Koordináta fordító!!!/////
 
-    public static int[] coordinateInterpreter(String coordinate){
+    public static int[] coordinateInterpreter(String coordinate) {
         int[] returnArray = new int[2];
         char[] charArray = coordinate.toCharArray();
-        for (int i = 0; i<charArray.length; i++){
-            if (charArray[0]>=65&&charArray[0]<=74){
-                returnArray[0]=charArray[0]-65;
+        for (int i = 0; i < charArray.length; i++) {
+            if (charArray[0] >= 65 && charArray[0] <= 74) {
+                returnArray[0] = charArray[0] - 65;
             }
-            if (charArray[1]>=48&&charArray[1]<=57){
-                returnArray[1]=charArray[1]-49;
+            if (charArray[1] >= 48 && charArray[1] <= 57) {
+                returnArray[1] = charArray[1] - 49;
             }
         }
         return returnArray;
     }
 
 
-    public void printoutTable(){
-        System.out.println(this.getClass().getSimpleName());
-        System.out.println("");
-        System.out.println("-------------------------------------------");
-        for(int count = 0; count < 10; count++){
-            System.out.print(" | ");
-                for(int count2 = 0; count2 < 10; count2++) {
+    public void printoutTable() {
+            System.out.println(this.getClass().getSimpleName());
+            System.out.println("");
+            System.out.println("  | A | B | C | D | E | F | G | H | I | J |");
+            System.out.println("-------------------------------------------");
+            for (int count = 0; count < 10; count++) {
+                if(count != 9) {
+                    System.out.print(count + 1 + " | ");
+                } else {
+                    System.out.print(count + 1 + "| ");
+                }
+                for (int count2 = 0; count2 < 10; count2++) {
                     System.out.print(table[count][count2] + " | ");
                 }
 
-            System.out.println();
+                System.out.println();
                 System.out.println("-------------------------------------------");
             }
         }
-}
+    }
+
+
 
