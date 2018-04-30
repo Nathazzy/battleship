c9b7c696bc925c41a1d99db4bec1df462d9c819c
zsolthomework <zsolt@mail.hu>
2018-04-30 13:58:49 +0200

    Sound method added

---
+++ src/main/java/com/matritellabs/utama/Shot.java
@@ -0,0 +1,30 @@
+package main.java.com.matritellabs.utama;
+
+
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+
+import java.io.FileInputStream;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Shot {
+
+
+    public static void play() {
+        try {
+            // open the sound file as a Java input stream
+            InputStream gongFile =  new FileInputStream("C:\\cygwin64\\home\\Zsoltlaptop\\battleship\\src\\ShotGun.wav");
+
+            // create an audiostream from the inputstream
+            AudioStream audioStream = new AudioStream(gongFile);
+
+            // play the audio clip with the audioplayer class
+            AudioPlayer.player.start(audioStream);
+        }catch (  IOException e) {
+            System.err.println("Something wrong");
+        }
+    }
+}
+
