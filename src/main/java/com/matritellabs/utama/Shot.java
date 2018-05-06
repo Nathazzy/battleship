f3fe9cc1bb01a726542d5d193518c064fed407bd
zsolthomework <zsolt@mail.hu>
2018-05-06 16:47:23 +0200

    Game starting sound added

--- src/main/java/com/matritellabs/utama/Shot.java
+++ src/main/java/com/matritellabs/utama/Shot.java
@@ -23,8 +23,24 @@ public class Shot {
             // play the audio clip with the audioplayer class
             AudioPlayer.player.start(audioStream);
         }catch (  IOException e) {
-            System.err.println("Something wrong");
+            System.err.println("Something wrong with audioplayer");
         }
     }
+    public static void start() {
+        try {
+            // open the sound file as a Java input stream
+            InputStream gongFile =  new FileInputStream("src\\Strat.wav");
+
+            // create an audiostream from the inputstream
+            AudioStream audioStream = new AudioStream(gongFile);
+
+            // play the audio clip with the audioplayer class
+            AudioPlayer.player.start(audioStream);
+        }catch (  IOException e) {
+            System.err.println("Something wrong with audioplayer");
+        }
+    }
+
+
 }
 
